1b4bfcb2864e112a5c3ffeae39ea617c46a0bb33
david2611 <david.r.hall.2611@gmail.com>
2019-09-19 14:24:36 +1000

    Added functionality for background_idx None

--- pdq.py
+++ pdq.py
@@ -350,9 +350,12 @@ def _calc_qual_img(gt_instances, det_instances, background_idx=0):
         FN = 0
         # Handle false positives in the image (different levels of fp cost)
         if len(det_instances) > 0:
-            tot_fp_cost = np.sum([np.max(list(det_instance.class_list[:background_idx]) +
-                                         list(det_instance.class_list[background_idx+1:]))
-                                  for det_instance in det_instances])
+            if background_idx is None:
+                tot_fp_cost = np.sum([np.max(det_instance.class_list) for det_instance in det_instances])
+            else:
+                tot_fp_cost = np.sum([np.max(list(det_instance.class_list[:background_idx]) +
+                                             list(det_instance.class_list[background_idx+1:]))
+                                      for det_instance in det_instances])
 
         # Filter out GT instances which are to be ignored because they are too small
         elif len(gt_instances) > 0:
@@ -410,8 +413,11 @@ def _calc_qual_img(gt_instances, det_instances, background_idx=0):
     tot_tp_label_quality = np.sum(label_quality_table[row_idxs, col_idxs])
 
     # Calculate the penalty for assigning a high label probability to false positives
-    tot_fp_cost = np.sum([np.max(list(det_instances[i].class_list[:background_idx]) +
-                                 list(det_instances[i].class_list[background_idx+1:])) for i in false_positive_idxs])
+    if background_idx is None:
+        tot_fp_cost = np.sum([np.max(det_instances[i].class_list) for i in false_positive_idxs])
+    else:
+        tot_fp_cost = np.sum([np.max(list(det_instances[i].class_list[:background_idx]) +
+                                     list(det_instances[i].class_list[background_idx+1:])) for i in false_positive_idxs])
 
     return {'overall': tot_overall_img_quality, 'spatial': tot_tp_spatial_quality, 'label': tot_tp_label_quality,
             'TP': true_positives, 'FP': false_positives, 'FN': false_negatives, 'fp_cost': tot_fp_cost}
